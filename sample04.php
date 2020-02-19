995faaaed9a3bd110eafdabb8c36f3b8290879fa
kktakano <kktakano03@gmail.com>
2020-02-19 22:46:20 +0900

    Update sample04.php

--- sample04.php
+++ sample04.php
@@ -19,6 +19,11 @@
 <h2>Practice</h2>
 <pre>
 <!-- ここにプログラムを記述します -->
+<?php
+date_default_timezone_set('Asia/Tokyo');
+$today = new DateTime();
+print($today->format('G時 i分 s秒'))
+?>
 </pre>
 </main>
 </body>    
